b06be459c5eb93d5f6331a8efe90be6ebc84fd9f
Jon White <jonwhite@jons-imac.home>
2020-12-10 19:07:32 +0000

    removed old understrap-child to desktop and adapting bucks to this

--- wp-content/themes/understrap-child/dist/blocks/block-carousel-full-page.php
+++
@@ -1,9 +0,0 @@
-<div class="jumbotron jumbotron-carousel">
-	<div class="jumbotron-bg">
-		<?php block_field( 'carousel-short-code' ); ?>
-	</div>
-	<div class="jumbotron-main">
-		<h1 class="animated fadeInDownBig"><?php block_field( 'jumbotron-h1' ); ?></h1>
-		<h2 class="animated fadeInUp"><?php block_field( 'jumbotron-strapline' ); ?></h2>
-	</div>
-</div>
